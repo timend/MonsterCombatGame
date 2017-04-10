57da3a1c2bfe8c74c897b8c5746559f786e4bd98
Tim Enderling <tim@zollsoft.de>
2017-04-10 16:31:50 +0200

    Initial commit.

---
+++ core/src/de/enderling/monstercombat/MonsterCombatGame.java
@@ -0,0 +1,451 @@
+package de.enderling.monstercombat;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.*;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Timer;
+import net.dermetfan.gdx.maps.tiled.TmxMapWriter;
+import net.dermetfan.gdx.maps.tiled.TmxMapWriter.Format;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class MonsterCombatGame extends ApplicationAdapter implements InputProcessor {
+    Texture img;
+    OrthographicCamera camera;
+    TiledMapRenderer tiledMapRenderer;
+
+    TiledMapTileLayer moveableLayer;
+    TiledMapTileLayer groundLayer;
+    TiledMap tiledMap;
+    List<Character> monsters;
+    Character player;
+    Timer timer;
+    Random random;
+    Stage stage;
+
+    Label destroyStonesLabel;
+
+    Skin skin;
+
+
+    public class GameState {
+        String fileName;
+        int destroyStones;
+
+        public GameState(String fileName, int destroyStones) {
+            this.fileName = fileName;
+            this.destroyStones = destroyStones;
+        }
+    }
+
+    GameState savePoint = null;
+
+    @Override
+    public void create () {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        stage = new Stage();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,w,h);
+        camera.update();
+
+        addObservedKey(Input.Keys.LEFT);
+        addObservedKey(Input.Keys.RIGHT);
+        addObservedKey(Input.Keys.UP);
+        addObservedKey(Input.Keys.DOWN);
+        Gdx.input.setInputProcessor(this);
+
+        skin = new Skin();
+
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        skin.add("default", new BitmapFont());
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = skin.getFont("default");
+        skin.add("default", labelStyle);
+
+        destroyStonesLabel = new Label("0", skin);
+        destroyStonesLabel.setX(w-20);
+        destroyStonesLabel.setY(h-20);
+
+        stage.addActor(destroyStonesLabel);
+
+        this.timer = new Timer();
+        this.random = new Random();
+
+        loadGameState(new GameState("julius.tmx", 0));
+
+        savePoint();
+    }
+
+    private void savePoint() {
+        File savePointFile = null;
+        FileWriter writer = null;
+        try {
+            savePointFile = new File("savePoint" + System.currentTimeMillis() + ".tmx");
+            writer = new FileWriter(savePointFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            new TmxMapWriterFixed(writer).tmx(tiledMap, Format.CSV);
+            savePoint = new GameState(savePointFile.getPath(), destroyStones);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void loadGameState(GameState gameState) {
+        timer.clear();
+
+        tiledMap = new TmxMapLoader().load(gameState.fileName);
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        moveableLayer = (TiledMapTileLayer) tiledMap.getLayers().get("Monster");
+        groundLayer = (TiledMapTileLayer) tiledMap.getLayers().get("Boden");
+
+        player = findPlayer();
+
+
+        camera.position.set((player.getX())*32, (player.getY()) * 32, 0);
+
+        monsters = findCharacters("monster");
+
+        for (Character monster : monsters) {
+            monster.init();
+        }
+
+        destroyStones = gameState.destroyStones;
+        destroyStonesLabel.setText(Integer.toString(destroyStones));
+    }
+
+    private Character findPlayer() {
+        return findCharacters("spieler").get(0);
+    }
+
+    @Override
+    public void render () {
+        processKeys();
+
+        stage.act();
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+        stage.draw();
+    }
+
+    public void addObservedKey(int key) {
+        observedKeys.put(key, new ObservedKey(key));
+    }
+
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.NUM_1)
+            tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+        if(keycode == Input.Keys.NUM_2)
+            tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+        if(keycode == Input.Keys.NUM_3)
+            tiledMap.getLayers().get(2).setVisible(!tiledMap.getLayers().get(2).isVisible());
+        if (keycode == Input.Keys.SPACE)
+            loadGameState(savePoint);
+        return false;
+    }
+
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    private static class ObservedKey {
+        private int key;
+        private float pressedFor;
+        private boolean pressed;
+        private static float repeatFrequency = 0.3f;
+
+        public ObservedKey(int key) {
+            this.key = key;
+            this.pressed = false;
+            this.pressedFor = 0;
+        }
+
+        public void observe() {
+            if(Gdx.input.isKeyPressed(key)) {
+                if (pressed) {
+                    pressedFor += Gdx.graphics.getRawDeltaTime();
+                } else {
+                    pressed = true;
+                    pressedFor = 0;
+                }
+            } else {
+                pressed = false;
+            }
+        }
+
+        public boolean justPressedOrRepeated() {
+            if (!pressed) {
+                return false;
+            }
+
+            if (pressedFor == 0) {
+                return true;
+            }
+
+            if (pressedFor > repeatFrequency) {
+                pressedFor -= repeatFrequency;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    private Map<Integer, ObservedKey> observedKeys = new HashMap<Integer, ObservedKey>();
+
+    public boolean processKeys() {
+        for (ObservedKey observedKey : observedKeys.values()) {
+            observedKey.observe();
+        }
+
+        if(observedKeys.get(Input.Keys.LEFT).justPressedOrRepeated())
+            movePlayer(-1, 0);
+        if(observedKeys.get(Input.Keys.RIGHT).justPressedOrRepeated())
+            movePlayer(1, 0);
+        if(observedKeys.get(Input.Keys.UP).justPressedOrRepeated())
+            movePlayer(0, 1);
+        if(observedKeys.get(Input.Keys.DOWN).justPressedOrRepeated())
+            movePlayer(0, -1);
+        return false;
+    }
+
+    private void movePlayer(int dx, int dy) {
+        if (moveCharacter(dx, dy, player, true)) {
+            camera.translate(32 * dx, 32 * dy);
+
+            TiledMapTileLayer.Cell cell = groundLayer.getCell(player.getX(), player.getY());
+
+            if (cell != null) {
+                if (cell.getTile().getProperties().containsKey("speichern")) {
+                    Integer gespeichert = cell.getTile().getProperties().get("gespeichert", 0, Integer.class);
+                    cell.setTile(tiledMap.getTileSets().getTileSet("dungeon").getTile(gespeichert+1));
+                    savePoint();
+                }
+            }
+        }
+
+    }
+
+    int destroyStones = 0;
+
+    /**
+     * Returns if the character moved.
+     *
+     * @param dx
+     * @param dy
+     * @param character
+     * @param canPush
+     * @return
+     */
+    public boolean moveCharacter(int dx, int dy, Character character, boolean canPush) {
+        int newPlayerX = character.getX()+dx;
+        int newPlayerY = character.getY()+dy;
+
+        if (newPlayerX < 0 || newPlayerX >= moveableLayer.getWidth()) {
+            return false;
+        }
+
+        if (newPlayerY < 0 || newPlayerY >= moveableLayer.getHeight()) {
+            return false;
+        }
+
+        if (!handleBlockingCell(dx, dy, canPush, newPlayerX, newPlayerY)) {
+            return false;
+        }
+
+        moveableLayer.setCell(character.getX(), character.getY(), null);
+        character.setX(newPlayerX);
+        character.setY(newPlayerY);
+        moveableLayer.setCell(character.getX(), character.getY(), character.getCell());
+        return true;
+    }
+
+    /**
+     * Returns if the blocking cell got dissolved (or there was none at the first place)
+     *
+     * @param dx
+     * @param dy
+     * @param canPush
+     * @param newPlayerX
+     * @param newPlayerY
+     * @return
+     */
+    private boolean handleBlockingCell(int dx, int dy, boolean canPush, int newPlayerX, int newPlayerY) {
+        TiledMapTileLayer.Cell blockingCell = moveableLayer.getCell(newPlayerX, newPlayerY);
+        if (blockingCell == null) {
+            return true;
+        }
+
+        if (!canPush) {
+            return false;
+        }
+
+        MapProperties blockingCellProperties = blockingCell.getTile().getProperties();
+        if (blockingCellProperties.containsKey("zauber")) {
+            destroyStones += blockingCellProperties.get("zauber", 0, Integer.class);
+            destroyStonesLabel.setText(Integer.toString(destroyStones));
+            return true;
+        }
+
+        if (!blockingCellProperties.containsKey("verschiebbar")) {
+            return false;
+        }
+
+        if (destroyStones > 0) {
+            destroyStones--;
+            destroyStonesLabel.setText(Integer.toString(destroyStones));
+            return true;
+        }
+
+        Character pushedCharacter = new Character(blockingCell, newPlayerX, newPlayerY);
+
+        return moveCharacter(dx, dy, pushedCharacter, canPush);
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+
+    public List<Character> findCharacters(String propertyName) {
+        List<Character> characters = new ArrayList<Character>();
+
+        for (int x = 0; x < moveableLayer.getWidth(); x++) {
+            for (int y = 0; y < moveableLayer.getHeight(); y++)  {
+                TiledMapTileLayer.Cell cell = moveableLayer.getCell(x, y);
+
+                if (cell != null && cell.getTile() != null) {
+                    if (cell.getTile().getProperties().containsKey(propertyName)) {
+                        characters.add(new Character(cell, x, y));
+                    }
+                }
+            }
+        }
+        return characters;
+    }
+
+    private class Character {
+        private TiledMapTileLayer.Cell cell;
+        private int x;
+        private int y;
+
+        public Character(TiledMapTileLayer.Cell cell, int x, int y) {
+            this.cell = cell;
+            this.x = x;
+            this.y = y;
+        }
+
+        public TiledMapTileLayer.Cell getCell() {
+            return cell;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setCell(TiledMapTileLayer.Cell cell) {
+            this.cell = cell;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public void init() {
+            float geschwindigkeit = cell.getTile().getProperties().get("schnell", 1.5f, Float.class);
+            Timer.Task task = new Timer.Task() {
+                @Override
+                public void run() {
+                    if (moveableLayer.getCell(x, y) != cell) {
+                        this.cancel();
+                    }
+
+                    if (random.nextFloat() > 0.6) {
+                        if (random.nextBoolean()) {
+                            moveCharacter(random.nextInt(3) - 1, 0, Character.this, false);
+                        } else {
+                            moveCharacter(0, random.nextInt(3) - 1, Character.this, false);
+                        }
+                    }
+                }
+            };
+            timer.scheduleTask(task, 0, 1/geschwindigkeit);
+        }
+    }
+}
---
+++ core/src/de/enderling/monstercombat/TmxMapWriterFixed.java
@@ -0,0 +1,132 @@
+package de.enderling.monstercombat;
+
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pools;
+import net.dermetfan.gdx.maps.tiled.TmxMapWriter;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+
+import static com.badlogic.gdx.math.MathUtils.round;
+import static net.dermetfan.gdx.maps.MapUtils.getProperty;
+
+
+public class TmxMapWriterFixed extends TmxMapWriter {
+    /**
+     * creates a new {@link TmxMapWriter} using the given {@link Writer}
+     *
+     * @param writer
+     */
+    public TmxMapWriterFixed(Writer writer) {
+        super(writer);
+    }
+
+    @Override
+    public TmxMapWriter tmx(TiledMapTileSet set) throws IOException {
+        MapProperties props = set.getProperties();
+        element("tileset");
+        attribute("firstgid", getProperty(props, "firstgid", 1));
+        attribute("name", set.getName());
+        attribute("tilewidth", getProperty(props, "tilewidth", 0));
+        attribute("tileheight", getProperty(props, "tileheight", 0));
+        float spacing = getProperty(props, "spacing", Float.NaN), margin = getProperty(props, "margin", Float.NaN);
+        if(!Float.isNaN(spacing))
+            attribute("spacing", round(spacing));
+        if(!Float.isNaN(margin))
+            attribute("margin", round(margin));
+
+        Iterator<TiledMapTile> iter = set.iterator();
+        if(iter.hasNext()) {
+            TiledMapTile tile = iter.next();
+            element("tileoffset");
+            attribute("x", round(tile.getOffsetX()));
+            attribute("y", round(-tile.getOffsetY()));
+            pop();
+        }
+
+        element("image");
+        attribute("source", getProperty(props, "imagesource", ""));
+        attribute("imagewidth", getProperty(props, "imagewidth", 0));
+        attribute("imageheight", getProperty(props, "imageheight", 0));
+        pop();
+
+        iter = set.iterator();
+        if(iter.hasNext()) {
+            @SuppressWarnings("unchecked")
+            Array<String> asAttributes = Pools.obtain(Array.class);
+            asAttributes.clear();
+            boolean elementEmitted = false;
+            for(TiledMapTile tile = iter.next(); iter.hasNext(); tile = iter.next()) {
+                MapProperties tileProps = tile.getProperties();
+                for(String attribute : asAttributes)
+                    if(tileProps.containsKey(attribute)) {
+                        if(!elementEmitted) {
+                            element("tile");
+                            elementEmitted = true;
+                        }
+                        attribute(attribute, tileProps.get(attribute));
+                    }
+
+                if (tile.getProperties().getValues().hasNext()) {
+                    element("tile");
+                    attribute("id", tile.getId()-1);
+                    tmx(tileProps, asAttributes);
+                    pop();
+                }
+            }
+            asAttributes.clear();
+            Pools.free(asAttributes);
+            if(elementEmitted)
+                pop();
+        }
+
+        pop();
+        return this;
+    }
+
+    @Override
+    public TmxMapWriter tmx(MapProperties properties, Array<String> exclude) throws IOException {
+        Iterator<String> keys = properties.getKeys();
+        if(!keys.hasNext())
+            return this;
+
+        boolean elementEmitted = false;
+        while(keys.hasNext()) {
+            String key = keys.next();
+            if(exclude != null && exclude.contains(key, false))
+                continue;
+            if(!elementEmitted) {
+                element("properties");
+                elementEmitted = true;
+            }
+
+            String type = null;
+            String className = properties.get(key).getClass().getSimpleName();
+
+            if (className.equals("Boolean")) {
+                type = "bool";
+            } else if (className.equals("Float")) {
+                type = "float";
+            } else if (className.equals("Integer")) {
+                type = "int";
+            } else if (className.equals("String")) {
+                type = null;
+            }
+
+            element("property").attribute("name", key).attribute("value", properties.get(key));
+
+            if (type != null) {
+                attribute("type", type);
+            }
+            pop();
+        }
+
+        if(elementEmitted)
+            pop();
+        return this;
+    }
+}

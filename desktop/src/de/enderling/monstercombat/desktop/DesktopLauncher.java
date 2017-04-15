a3bf821bb552c9e22abd9f1a58ad7d3277e10f18
Tim Enderling <tim@zollsoft.de>
2017-04-15 23:48:08 +0200

    Increase size of window.

--- desktop/src/de/enderling/monstercombat/desktop/DesktopLauncher.java
+++ desktop/src/de/enderling/monstercombat/desktop/DesktopLauncher.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.enderling.monstercombat.MonsterCombatGame;
 
+import java.awt.*;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 768;
 		new LwjglApplication(new MonsterCombatGame(), config);
 	}
 }
